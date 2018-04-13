47723962d10e014d953eb3c877523bdfeb14bb09
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2018-04-12 22:58:02 -0700

    Rename variables and fix bug where incorrect number of Fib numbers would be calculated

--- fib.py
+++ fib.py
@@ -2,10 +2,10 @@
 import decimal
 decimal.getcontext().prec = 150
 
-def calc_fib_constant(n, iter):
+def calc_fib_constant(n, count):
 	# Here, n is the order of the Fibonacci sequence
 	# n=2 corresponds to the classic Fibonacci sequence
-	# iter is the number of iterations to go through
+	# count is the number of Fibonacci numbers to generate
 	# TODO: Make it work for n != 2
 
 	# Starting numbers
@@ -16,13 +16,13 @@ def calc_fib_constant(n, iter):
 	fib_nums = []
 
 	# Populate the list with init values
-	for i in range(n):
+	for i in range(n - 1):
 		fib_nums.append(a)
 	fib_nums.append(b)
 
 	# Now to calculate the rest of the sequence
 	# TODO: Generalize this for n != 2
-	for i in range(iter - 3):
+	for i in range(count - 2):
 
 		# print("i: " + str(i))
 
@@ -36,14 +36,14 @@ def calc_fib_constant(n, iter):
 		# print("next_num: " + str(next_num))
 		fib_nums.append(next_num)
 
-	# print(fib_nums)
+	print(fib_nums)
 
 	r = decimal.Decimal(fib_nums[len(fib_nums) - 1]) / decimal.Decimal(fib_nums[len(fib_nums) - 2])
 	print(r)
 
 # Regular old Fibonacci sequence
 calc_fib_constant(2, 10)
-calc_fib_constant(2, 100)
-calc_fib_constant(2, 1000)
-calc_fib_constant(2, 10000)
-calc_fib_constant(2, 100000)
+# calc_fib_constant(2, 100)
+# calc_fib_constant(2, 1000)
+# calc_fib_constant(2, 10000)
+# calc_fib_constant(2, 100000)
